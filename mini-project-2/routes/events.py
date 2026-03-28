c854d517c5ff3e5e35769f1db59130756c8ee718
Selin Turkdogan <turkdogan.selinn@gmail.com>
2026-03-28 21:18:01 +0300

    implement get all events endpoint

--- mini-project-2/routes/events.py
+++ mini-project-2/routes/events.py
@@ -7,3 +7,8 @@ router = APIRouter()
 async def create_event(event: Event):
     await event.insert()
     return event
+
+@router.get("/events")
+async def get_events():
+    events = await Event.find().to_list()
+    return events
