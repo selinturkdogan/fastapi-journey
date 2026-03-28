bd8dd3a55f05c54385c31e63984c1a77d819b7b7
Selin Turkdogan <turkdogan.selinn@gmail.com>
2026-03-28 21:22:14 +0300

    implement get event by id endpoint

--- mini-project-2/routes/events.py
+++ mini-project-2/routes/events.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from models.events import Event
+from fastapi import HTTPException
 
 router = APIRouter()
 
@@ -12,3 +13,12 @@ async def create_event(event: Event):
 async def get_events():
     events = await Event.find().to_list()
     return events
+
+@router.get("/events/{event_id}")
+async def get_event(event_id: str):
+    event = await Event.get(event_id)
+
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+
+    return event
