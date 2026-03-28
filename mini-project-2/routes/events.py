f300bfdb5f6a4f2b4c63bd3d5e2c21082e672978
Selin Turkdogan <turkdogan.selinn@gmail.com>
2026-03-28 21:44:11 +0300

    mini-project-2: implement event update endpoint

--- mini-project-2/routes/events.py
+++ mini-project-2/routes/events.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter
-from models.events import Event
-from fastapi import HTTPException
+from fastapi import APIRouter, HTTPException
+from models.events import Event, EventUpdate
 
 router = APIRouter()
+
 
 @router.post("/events")
 async def create_event(event: Event):
     await event.insert()
     return event
 
+
 @router.get("/events")
 async def get_events():
     events = await Event.find().to_list()
     return events
+
 
 @router.get("/events/{event_id}")
 async def get_event(event_id: str):
@@ -23,6 +25,7 @@ async def get_event(event_id: str):
 
     return event
 
+
 @router.delete("/events/{event_id}")
 async def delete_event(event_id: str):
     event = await Event.get(event_id)
@@ -31,22 +34,21 @@ async def delete_event(event_id: str):
         raise HTTPException(status_code=404, detail="Event not found")
 
     await event.delete()
-
     return {"message": "Event deleted successfully"}
 
+
+
 @router.put("/events/{event_id}")
-async def update_event(event_id: str, updated_event: Event):
+async def update_event(event_id: str, body: EventUpdate):
     event = await Event.get(event_id)
 
     if not event:
         raise HTTPException(status_code=404, detail="Event not found")
 
-    event.title = updated_event.title
-    event.image = updated_event.image
-    event.description = updated_event.description
-    event.tags = updated_event.tags
-    event.location = updated_event.location
+    update_data = body.dict(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(event, key, value)
 
     await event.save()
-
     return event
